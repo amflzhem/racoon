ac57e91e4a49a2cf31cc57fa9199c4ca1a5f1b27
amflzhem <51090362+amflzhem@users.noreply.github.com>
2020-11-29 21:18:52 +0900

    첫번째 커밋

---
+++ js/loading.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function(){
+    setTimeout(function(){
+        determineType(calculateTest());
+    }, 3500);
+});
+
+function calculateTest(){
+    let finalScore = 0;
+    const testKeys = Object.keys(sessionStorage);
+
+    testKeys.forEach(function(e){
+        if(!isNumber(e)){
+            return;
+        }
+
+        if(e.length > 1){
+            return;
+        }
+
+        finalScore += Number(sessionStorage.getItem(e)); 
+    });
+
+    return finalScore;
+}
+
+function determineType(score){
+    let type;
+    
+    if(score <= 4){
+        type = "rest";
+    }else if(scoe <= 9){
+        type = "";
+    }else{
+        type = "";
+    }
+
+    location.href = type + ".html";
+}
+
+function isNumber(value){
+    return Number(value) !== NaN;
+}
