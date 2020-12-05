cacc1299db29f720b0a6d89b77bc1434ef520197
amflzhem <amflzhem@naver.com>
2020-12-05 19:14:44 +0900

    테스트 결과에 맞는 공유하기 화면

--- js/result.js
+++ js/result.js
@@ -1,27 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
+  const webURL = "https://amflzhem.github.io/racoon";
+
+  document.addEventListener("DOMContentLoaded", function() {
     Kakao.init("d0330e39cac07ecabf61be123b249810");
 
     Kakao.Link.createDefaultButton({
         container: "#share",
         objectType: "feed",
         content: {
-          title: "나의 너구리 유형은?",
+          title: "나의 너구리 유형은 " + document.getElementsByClassName("header")[0].innerText + "!",
           description: "당신과 꼭 맞는 너구리 유형을 찾아보세요!",
-          imageUrl: "https://amflzhem.github.io/racoon/images/%EA%B0%80%EC%9D%84%EB%84%88%EA%B5%AC%EB%A6%AC.jpg",
+          imageUrl: document.getElementsByTagName("img")[0].src,
           link: {
-            mobileWebUrl: "https://amflzhem.github.io/racoon"
+            mobileWebUrl: webURL
           }
         },
         buttons: [
           {
             title: "테스트 해보기",
             link: {
-              mobileWebUrl: "https://amflzhem.github.io/racoon"
+              mobileWebUrl: webURL
             }
           }
         ]
       });   
 });
-
- 
 
